5e670b0958cc39a59f3940e62ef13ac3699e0a3c
zzr716 <z71634@163.com>
2017-11-04 21:12:12 +0800

    first commit

--- js/index.js
+++ js/index.js
@@ -1,3 +1,8 @@
+/**
+ * 扇贝小作业
+ * By zzr
+ * 2017-11-04
+ */
 function removeOption() {
     var removeObjs = ['#bannerandheader', 'footer', '.content-footer', '.contributions__epic', '.submeta', '.content__section-label', '.content__meta-container', '.content__secondary-column', '.element', '.js-ad-slot', 'ad-slot']
     for (item in removeObjs) {
