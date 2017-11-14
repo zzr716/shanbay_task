611bba7637da0f3e34a50ba96c7cf34a4550f785
zzr716 <z71634@163.com>
2017-11-14 23:38:59 +0800

    change

--- js/index.js
+++ js/index.js
@@ -14,3 +14,25 @@ function removeOption() {
 }
 removeOption()
 setInterval(removeOption, 2000)
+/**
+ * 分页函数
+ * 数据行全部加载，通过是否显示属性完成分页功能
+ */
+function goPage() {
+    var acontent = document.querySelector('.content__article-body')
+    acontent.style.display = 'none'
+    // var pno = Math.ceil(parseInt(acontent.scrollHeight)/parseInt(acontent.offsetHeight))
+    var content = acontent.innerHTML
+    // 总行数
+    var allLine = Math.ceil(parseInt(content.length)/parseInt(acontent.clientWidth))
+        
+    // 翻页栏
+    var showContent = document.createElement('div')
+    var contentMain = document.querySelector('.l-side-margins')
+    contentMain.appendChild(showContent)
+    contentMain.style.position = 'relative'
+    showContent.style.position = 'absolute'
+    showContent.style.bottom = 0
+    showContent.innerHTML = '<a href="javascript:void(0)" onclick="DHTMLpagenation.previous()">上一页</a>'+'<a href="javascript:void(0)" onclick="DHTMLpagenation.next()">下一页</a>'
+    
+}
